66bc12b070573eb9791115cf6d33cd0b4944dcaf
Anup DSouza <anup.dsouza@gmail.com>
2024-05-10 16:02:54 +0530

    Add initial files

---
+++ GeminiTicTacToe/GeminiTicTacToe/GeminiTicTacToeApp.swift
@@ -0,0 +1,21 @@
+//
+//  GeminiTicTacToeApp.swift
+//  GeminiTicTacToe
+//
+//  Created by Anup D'Souza on 10/05/24.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/anupdsouza
+//
+
+import SwiftUI
+
+@main
+struct GeminiTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
