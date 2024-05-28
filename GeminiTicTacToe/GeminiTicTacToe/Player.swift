4996719dd9dd1c425e222faa7f187cf1a360bb1b
Anup DSouza <anup.dsouza@gmail.com>
2024-05-28 16:45:37 +0530

    Add player model

---
+++ GeminiTicTacToe/GeminiTicTacToe/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  GeminiTicTacToe
+//
+//  Created by Anup D'Souza on 28/05/24.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/anupdsouza
+//
+
+import Foundation
+
+enum Player {
+    case human
+    case computer
+}
