53a0c47c927ec82effe2b436991e47c63b420020
Anup DSouza <anup.dsouza@gmail.com>
2024-05-28 19:04:55 +0530

    Make vacant positions flash during ai turn & optimised code

--- GeminiTicTacToe/GeminiTicTacToe/ContentView.swift
+++ GeminiTicTacToe/GeminiTicTacToe/ContentView.swift
@@ -12,10 +12,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    private var columns = [GridItem].init(repeating: GridItem(.flexible()), count: 3)
+    private var columns = [GridItem](repeating: GridItem(.flexible()), count: 3)
     private let spacing: CGFloat = 20
-    @State private var turns = [Turn?].init(repeating: nil, count: 9)
+    @State private var turns = [Turn?](repeating: nil, count: 9)
+    @State private var opacities = [Double](repeating: 1.0, count: 9)
+    @State private var timers = [Timer?](repeating: nil, count: 9)
     @State private var currentPlayer: Player = .human
+    @State private var vacantPositions: [Int] = []
 
     var body: some View {
         VStack {
@@ -26,17 +29,23 @@ struct ContentView: View {
                         MarkerItemView(mark: turns[index]?.mark ?? "")
                             .frame(width: geometry.size.width/3 - spacing,
                                    height: geometry.size.width/3 - spacing)
+                            .opacity(turns[index] == nil ? opacities[index] : 1)
                             .onTapGesture {
                                 if isValidTurn(position: index) {
                                     updateTurn(position: index)
                                     // TODO: check if player won, end game
                                     changePlayer()
                                     
+                                    // Start flashing during AI turn
+                                    startFlashing()
+                                    
                                     DispatchQueue.main.asyncAfter(wallDeadline: .now() + 5) {
-                                        guard let aiTurnPosition = aiTurn() else {
-                                            return
-                                        }
+                                        let aiTurnPosition = aiTurn()
+                                        
+                                        stopFlashing()
+                                        
                                         updateTurn(position: aiTurnPosition)
+                                        
                                         // TODO: check if ai won, end game
                                         changePlayer()
                                     }
@@ -52,22 +61,50 @@ struct ContentView: View {
     }
     
     private func isValidTurn(position: Int) -> Bool {
-        !turns.contains { $0?.position == position }
+        turns[position] == nil
     }
     
     private func updateTurn(position: Int) {
         turns[position] = .init(player: currentPlayer, position: position)
+        updateVacantPositions()
     }
 
     private func changePlayer() {
         currentPlayer = currentPlayer == .ai ? .human : .ai
     }
-    
-    private func aiTurn() -> Int? {
-        let openPositions = turns.enumerated().compactMap { index, element in
+
+    private func updateVacantPositions() {
+        vacantPositions = turns.enumerated().compactMap { index, element in
             element == nil ? index : nil
         }
-        return openPositions.randomElement()
+    }
+    
+    private func aiTurn() -> Int {
+        vacantPositions.randomElement()!
+    }
+    
+    private func startFlashing() {
+        stopFlashing()
+        
+        for index in vacantPositions {
+            timers[index] = Timer.scheduledTimer(withTimeInterval: Double.random(in: 0.3...0.6), repeats: true) { timer in
+                withAnimation {
+                    opacities[index] = (opacities[index] == 0.5) ? 1 : 0.5
+                }
+            }
+        }
+    }
+    
+    private func stopFlashing() {
+        for index in timers.indices {
+            timers[index]?.invalidate()
+            timers[index] = nil
+            opacities[index] = 1
+        }
+    }
+    
+    private func resetGame() {
+        // TODO: reset states
     }
 }
 
